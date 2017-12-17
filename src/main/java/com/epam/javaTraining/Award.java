8f68f6c662de471cee1977c81dfb17d09ec55ffd
VKavaliou1984 <vkovalev1984@gmail.com>
2017-12-17 20:19:38 +0300

    commit #3

---
+++ src/main/java/com/epam/javaTraining/Award.java
@@ -0,0 +1,19 @@
+package com.epam.javaTraining;
+
+/**
+ * Created by Viachaslau_Kavaliou on 17/12/2017.
+ */
+public class Award {
+private int value = 1000;
+
+public Award (final Integer value) {
+    this.value = value;
+}
+//public int getValue(){
+//    return value;
+//}
+
+public static void main (Integer value) {
+    System.out.println(value);
+}
+}
